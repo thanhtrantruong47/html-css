5f41c9c556ed2514a015e5d8b693253e8cac2e81
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-18 21:36:30 +0700

    create login

--- practice-two/src/index.js
+++ practice-two/src/index.js
@@ -1,17 +1,31 @@
-const plus = document.querySelector(".plus"),
-  minus = document.querySelector(".minus"),
-  num = document.querySelector(".num");
-let a = 1;
-plus.addEventListener("click", () => {
-  a++;
-  a = a < 10 ? "0" + a : a;
-  num.innerText = a;
-});
+// const plus = document.querySelector(".plus"),
+//   minus = document.querySelector(".minus"),
+//   num = document.querySelector(".num");
+// let a = 1;
+// plus.addEventListener("click", () => {
+//   a++;
+//   a = a < 10 ? "0" + a : a;
+//   num.innerText = a;
+// });
 
-minus.addEventListener("click", () => {
-  if (a > 1) {
-    a--;
-    a = a < 10 ? "0" + a : a;
-    num.innerText = a;
-  }
-})
+// minus.addEventListener("click", () => {
+//   if (a > 1) {
+//     a--;
+//     a = a < 10 ? "0" + a : a;
+//     num.innerText = a;
+//   }
+// })
+
+const myLink = document.querySelector(".login__link");
+const btnLogin = document.querySelector(".login__btn");
+const myTitle = document.querySelector('.login__title')
+const haveAccount = document.querySelector('.login__sign-in')
+const myDesc = document.querySelector('.login__desc')
+
+const changeForm = () => {
+  myTitle.textContent = "Sign In";
+  btnLogin.textContent= "Sign In";
+  haveAccount.textContent = "You have not account ?"
+  myLink.textContent = "create account";
+  myDesc.textContent = "Sign in your account"
+}
